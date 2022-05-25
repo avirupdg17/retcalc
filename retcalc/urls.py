c036737a280ae111db31fac2694ec715bf980af3
Avirup Dutta Gupta <avirupduttagupta@gmail.com>
2022-05-25 22:53:55 +0530

    Adding Calculator component

--- retcalc/urls.py
+++ retcalc/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('newuser/',include('usercreation.urls')),
+    path('calculate/',include('calculator.urls'))
 ]
